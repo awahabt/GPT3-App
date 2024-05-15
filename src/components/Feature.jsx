bcd9e1c6db61f00f9e60e246787a0b0f0ff4d7b9
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-05-15 20:41:26 +0500

    Update Feature.jsx

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { FeatureLinks } from "../index";
 
+// const Feature = ({title, description})=>(
+//   <div>
+//     <div className="gradient-div w-14 h-1"/>
+//     <h1>{title}</h1>
+//     <div>{description}</div>
+//   </div>
+// )
+
 const feature = () => {
   return (
-    <div className="flex mt-10 justify-evenly px-[8%] items-center">
-      <div className="gap-10">
-        <p className="gradient-text text-4xl leading-normal max-w-[470px] font-bold max-[401px]:text-2xl">
+    <div className="flex gap-16 flex-wrap my-[150px] justify-between fontmanrop max-[1196px]:justify-center max-[730px]:px-[8%]">
+      <div className="">
+        <p className="gradient-text text-4xl leading-normal max-w-[470px] font-extrabold max-[401px]:text-2xl">
           The Future is Now and You Just Need To Realize It. Step into Future
           Today & Make it Happen.
         </p>
@@ -14,9 +22,16 @@ const feature = () => {
         </a>
       </div>
 
-      <div className="justify-center">
-        {FeatureLinks.map((tit, desc) => (
-          <Feature title:{tit.title}/>
+      <div>
+        {FeatureLinks.map((tit) => (
+          <div className="flex justify-between fontmanrop gap-16 items-center">
+            <div className="text-white font-bold max-w-[180px] text-xl my-6 max-[401px]:text-xl">
+              <div className="gradient-div w-14 h-1 mb-2" />
+              <p className="text-white">{tit.title}</p>
+            </div>
+
+            <div className="text-dimWhite max-w-[370px] my-6">{tit.description}</div>
+          </div>
         ))}
       </div>
     </div>
