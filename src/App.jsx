01844e97fb2646a60732406dbef933d70df54f9c
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-05-17 18:39:24 +0500

    Added 'Hero' section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { NavLinks, Sponser, WhatsGPT3, Feature, Possibility, CTA, Blog, Footer} from "./components";
+import {
+  NavLinks,
+  Sponser,
+  WhatsGPT3,
+  Feature,
+  Possibility,
+  CTA,
+  Blog,
+  Footer,
+  Hero,
+} from "./components";
 
 const App = () => (
   <div className="bg w-full overflow-hidden">
     <div className="px-6 flex justify-center items-center max-[640px]:px-16">
       <div className="xl:max-w-[1380px] w-full">
         <NavLinks />
-        {/* <Hero/> */}
-        <Sponser/>
-        <WhatsGPT3/>
-        <Feature/>
-        <Possibility/>
-        <CTA/>
-        <Blog/>
-        <Footer/>
+        <Hero />
+        <Sponser />
+        <WhatsGPT3 />
+        <Feature />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
       </div>
     </div>
     <div className="bg-gradient w-[20%] h-[30%] absolute z-[2]" />
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,41 +1,42 @@
+import React from 'react'
 import { ai, people } from "../assets";
 
 const Hero = () => {
   return (
     <div className=" flex-wrap min-[1386px]:flex min-[1386px]:justify-between max-[1386px]:items-center main-div ">
-      <div className="fontmanrop mt-16 max-[1386px]:w-[100%] max-[1386px]:text-center min-[1386px]:w-[600px] max-[1386px]:items-center">
-        <h1 className="gradient-text max-[1386px]:text-center max-[471px]:text-[30px] text-6xl leading-normal  font-extrabold">
-          Let's Build Something amazing with GPT-3 OpenAI
-        </h1>
-        <p className="text-dimWhite text-[20px] max-[1386px]:text-center mt-10">
-          Yet bed any for travelling assistance indulgence unpleasing. Not
-          thoughts all exercise blessing. Indulgence way everything joy
-          alteration boisterous the attachment. Party we years to order allow
-          asked of.
-        </p>
-        <div className="mt-8 max-[601px]:flex-wrap max-[601px]:flex-column input-div">
-          <form className="flex">
-            <input
-              className="input"
-              type="email"
-              placeholder="Your Email Address"
-            />
-            <button className="button" type="submit">
-              Get Started
-            </button>
-          </form>
-        </div>
-
-        <div className="flex items-center text-white mt-8 max-[1386px]:flex-wrap max-[1386px]:justify-center max-[1386px]:gap-6">
-          <img src={people} alt="people" />
-          <p>1,600 people requested access a visit in last 24 hours</p>
-        </div>
+    <div className="fontmanrop mt-16 max-[1386px]:w-[100%] max-[1386px]:text-center min-[1386px]:w-[600px] max-[1386px]:items-center">
+      <h1 className="gradient-text max-[1386px]:text-center max-[471px]:text-[30px] text-6xl leading-normal  font-extrabold">
+        Let's Build Something amazing with GPT-3 OpenAI
+      </h1>
+      <p className="text-dimWhite text-[20px] max-[1386px]:text-center mt-10">
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy
+        alteration boisterous the attachment. Party we years to order allow
+        asked of.
+      </p>
+      <div className="mt-8 max-[601px]:flex-wrap max-[601px]:flex-column input-div">
+        <form className="flex">
+          <input
+            className="input"
+            type="email"
+            placeholder="Your Email Address"
+          />
+          <button className="button" type="submit">
+            Get Started
+          </button>
+        </form>
       </div>
-      <div className="ai-img">
-        <img src={ai} alt="AI-Logo" className=" w-[660px] " />
+
+      <div className="flex items-center text-white mt-8 max-[1386px]:flex-wrap max-[1386px]:justify-center max-[1386px]:gap-6">
+        <img src={people} alt="people" />
+        <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
-  );
-};
+    <div className="ai-img">
+      <img src={ai} alt="AI-Logo" className=" w-[660px] " />
+    </div>
+  </div>
+  )
+}
 
-export default Hero;
+export default Hero
